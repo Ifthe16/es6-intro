d478b5bfd568970c20b0eaf6a85fcfe060017dc7
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-30 02:04:56 +0600

    added keys file of javascript

---
+++ keys.js
@@ -0,0 +1,51 @@
+const glass = {
+    name: 'glass',
+    color: 'golden',
+    price: 12,
+    isCleaned: true
+};
+
+console.log(glass);
+
+// all properties name
+const keys = Object.keys(glass);
+console.log(keys);
+// [ 'name', 'color', 'price', 'isCleaned' ]
+
+// all property values
+const values = Object.values(glass);
+console.log(values);
+// [ 'glass', 'golden', 12, true ]
+
+const entries = Object.entries(glass);
+console.log(entries);
+// array of array, two dimensional array 
+// [
+//   [ 'name', 'glass' ],
+//   [ 'color', 'golden' ],
+//   [ 'price', 12 ],
+//   [ 'isCleaned', true ]
+// ]
+
+// delete
+// delete glass.isCleaned;
+// console.log(glass);
+
+// rest
+const { isCleaned, ...shortGlass } = glass;
+console.log(shortGlass);
+
+// freeze
+// Object.freeze(glass);
+// glass.source = 'Bangladesh';
+// glass.price = 5000;
+// delete glass.name;
+// console.log(glass);
+
+// seal
+Object.seal(glass);
+glass.source = 'Bangladesh';
+glass.price = 5000;
+delete glass.name;
+console.log(glass);
+
